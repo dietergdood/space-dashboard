34297b8f6a0f92cbbb73e5a08ecabaa2c11c4a98
dietergdood <dieter.good@outlook.com>
2026-06-01 01:37:08 +0200

    Add files via upload

--- api/history.js
+++ api/history.js
@@ -1,6 +1,8 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
 
   const { ticker, range } = req.query;
   if (!ticker || !/^[A-Z]{1,6}$/.test(ticker.toUpperCase())) {
